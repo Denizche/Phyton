c0effffaf113087653616bb76ee9292031dd3b6e
Denizche <31962763+Denizche@users.noreply.github.com>
2017-09-14 18:45:54 +0300

    Update arr_max_and_avg.py

--- arr_max_and_avg.py
+++ arr_max_and_avg.py
@@ -1,28 +1,41 @@
 #arr max and average
 #denis chechelev RT5-51
 def arr_min(array):
+  if len(array) == 0:
+      return -1
   minim = array[0]
   for i in range(1, len(array)):
-    crnt = array[i]
-    if crnt < minim:
-      minim = crnt
+    current = array[i]
+    if current < minim:
+      minim = current
+    
   return minim
 
-def arr_avg(array):
-  sm = 0
-  for i in range(len(array)):
-    sm+=array[i]
-  return sm / len(array)
 
-array1 = [-999,666,13,47,-27.7,-0.000001]
-array2 = [-0,1000,50,-50]     
-print(array1)
-print('Минимальное значение: ')
-print(arr_min(array1))  
-print('Среднее значение: ')
-print(arr_avg(array1))
-print(array2)
-print('Минимальное значение: ')
-print(arr_min(array2))  
-print('Среднее значение: ')
-print(arr_avg(array2))
+def arr_avg(array):
+  if len(array) == 0:
+      return -1
+  summ = 0
+  for i in range(len(array)):
+    summ += array[i]
+  return summ / len(array)
+  
+  
+def main():
+  array1 = [-999,666,13,47,-27.7,-0.000001]
+  array2 = []     
+  print(array1)
+  print('Минимальное значение: ')
+  print(arr_min(array1))  
+  print('Среднее значение: ')
+  print(arr_avg(array1))
+  print(array2)
+  print('Минимальное значение: ')
+  print(arr_min(array2))  
+  print('Среднее значение: ')
+  print(arr_avg(array2))
+  return 0
+
+print (__name__)
+if __name__ == 'builtins': #builtins вместо __main__
+  main()
